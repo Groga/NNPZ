0ff0536600b14eb4dc3a6203d7ef1c8232a1d31b
Groga <groga65@gmail.com>
2019-07-17 13:53:01 +0300

    Изменения в шаблонах

--- NNPZ/Ldap/views.py
+++ NNPZ/Ldap/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+# создание подключение к LDAP (AD)
+def Ldap_connect(request):
+
+    return render(request, 'Ldap/ldap_connect.html')
--- NNPZ/NNPZ/views.py
+++ NNPZ/NNPZ/views.py
@@ -7,4 +7,4 @@ def index(request):
 
     #latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #context = {'latest_question_list': latest_question_list}
-    return render(request, 'index.html')
+    return render(request, 'Portal.html')
